4b4d5ff39e5d04bd6d699c7af0b41e8f4c7dfa48
nfnino <nfnino@unbosque.edu.co>
2020-09-29 10:09:58 -0500

    Cambios UI

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const useraudits = require("./routes/api/audits/userAudits");
 
 const app = express();
 // DB Config
-const uri = 'mongodb://NicolasNino:233E169D@cluster0-shard-00-02-arsqt.mongodb.net:27017:27017,cluster0-shard-00-01-arsqt.mongodb.net:27017,cluster0-shard-00-00-arsqt.mongodb.net:27017/MERN?ssl=true&authSource=admin&retryWrites=true';
+const uri = 'mongodb://***user***:***pass***@cluster0-shard-00-02-arsqt.mongodb.net:27017:27017,cluster0-shard-00-01-arsqt.mongodb.net:27017,cluster0-shard-00-00-arsqt.mongodb.net:27017/MERN?ssl=true&authSource=admin&retryWrites=true';
 // Connect to MongoDB
 mongoose.connect(
     uri, 
