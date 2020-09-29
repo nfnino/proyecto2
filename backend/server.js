8ed08d9ecbfaee75e4ef59c5828117f122fb2f0d
nfnino <nfnino@unbosque.edu.co>
2020-09-29 12:10:45 -0500

    heroku prep 2

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const useraudits = require("./routes/api/audits/userAudits");
 
 const app = express();
 // DB Config
-const uri = `mongodb://NicolasNino:${proces.env.password}@cluster0-shard-00-02-arsqt.mongodb.net:27017:27017,cluster0-shard-00-01-arsqt.mongodb.net:27017,cluster0-shard-00-00-arsqt.mongodb.net:27017/MERN?ssl=true&authSource=admin&retryWrites=true`;
+const uri = `mongodb://NicolasNino:233E169D@cluster0-shard-00-02-arsqt.mongodb.net:27017:27017,cluster0-shard-00-01-arsqt.mongodb.net:27017,cluster0-shard-00-00-arsqt.mongodb.net:27017/MERN?ssl=true&authSource=admin&retryWrites=true`;
 // Connect to MongoDB
 mongoose.connect(
     uri, 
